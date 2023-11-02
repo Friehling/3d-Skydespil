69979a0c13545e0fa6f4687f4bd7870f39a78b66
Friehling <101862084+Friehling@users.noreply.github.com>
2023-11-02 15:26:59 +0100

    Player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -16,21 +17,62 @@ public class PlayerController : MonoBehaviour
 
     Rigidbody rb;
 
+    PhotonView PV;
+
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        PV = GetComponent<PhotonView>();
     }
 
     private void Update()
     {
-        transform.Rotate(Vector3.up*Input.GetAxisRaw("Mouse x")*mouseSensitivity);
+        if (PV.IsMine)
+            return;
 
-       
+        look();
+        Move();
+        Jump();
+        
+    }
 
+    void Move()
+    {
+        Vector3 moveDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
 
+        moveAmount = Vector3.SmoothDamp(moveAmount, moveDir * (Input.GetKey(KeyCode.LeftShift) ? sprintSpeed : walkSpeed), ref smoothMoveVelocity, smoothTime);
 
+        
+    }
+    void look()
+    {
+        transform.Rotate(Vector3.up * Input.GetAxisRaw("Mouse x") * mouseSensitivity);
+
+        verticalLockRotation += Input.GetAxisRaw("Mouse y") * mouseSensitivity;
+        verticalLockRotation = Mathf.Clamp(verticalLockRotation, -90f, 90f);
+
+        cameraHolder.transform.localEulerAngles = Vector3.left * verticalLockRotation;
 
     }
+
+    void Jump()
+    {
+        if (Input.GetKey(KeyCode.Space) && grounded)
+        {
+            rb.AddForce(transform.up * jumpForce);
+        }
+    }
+
+    public void SetGroundedState(bool _grounded)
+    {
+        grounded = _grounded;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position+transform.TransformDirection(moveAmount)*Time.fixedDeltaTime);
+    }
+
 
 
 }
