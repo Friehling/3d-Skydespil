a3083891997b04bb7a208a955d48711830da3666
Friehling <101862084+Friehling@users.noreply.github.com>
2023-11-22 12:49:42 +0100

    s

--- Assets/Scripts/Gun scripts/SingleShotGun.cs
+++ Assets/Scripts/Gun scripts/SingleShotGun.cs
@@ -1,3 +1,4 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,15 @@ using UnityEngine;
 public class SingleShotGun : Gun
 {
     [SerializeField] Camera cam;
+
+    PhotonView PV;
+
+    private void Awake()
+    {
+        PV = GetComponent<PhotonView>();
+    }
+
+
 
     public override void Use()
     {
@@ -18,6 +28,14 @@ public class SingleShotGun : Gun
         if(Physics.Raycast(ray, out RaycastHit hit))
         {
             hit.collider.gameObject.GetComponent<IDamageable>()?.TakeDamage(((GunInfo)itemInfo).damage);
+            PV.RPC("RPC_Shoot", RpcTarget.All, hit.point);
         }
     }
+    
+    [PunRPC]
+    void RPC_Shoot(Vector3 hitPosition)
+    {
+        Instantiate(bulletImpactPrefab, hitPosition, Quaternion.identity);
+    }
+
 }
