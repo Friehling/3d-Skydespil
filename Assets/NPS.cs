c1c2db14908d841cd408bfeb186da9da0e4b27f0
KnightOfFate <102350581+KnightOfFate@users.noreply.github.com>
2023-11-24 11:50:30 +0100

    bot

--- Assets/NPS.cs
+++ Assets/NPS.cs
@@ -7,27 +7,35 @@ using UnityEngine.AI;
 
 public class MultiplayerAgent : MonoBehaviour
 {
-    [SerializeField] private List<Transform> positions = new List<Transform>();
+    public NavMeshAgent agent;
 
-    private NavMeshAgent _agent;
-    private int positionIndex = 0;
+    public Transform player;
+
+    public LayerMask whatIsGround, whatIsPlayer;
+
+    public float health;
+
+    //Patroling
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+
+    //Attacking
+    public float timeBetweenAttacks;
+    bool alreadyAttacked;
 
     private void Awake()
     {
-        _agent = GetComponent<NavMeshAgent>();
+        player = GameObject.Find("PlayerController").transform;
+        agent = GetComponent<NavMeshAgent>();
     }
-
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.F))
-            NextPosition();
+        //Check for sight and attack range
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
 
+        if (!playerInSightRange && !playerInAttackRange) Patroling();
+        if (playerInSightRange && !playerInAttackRange) ChasePlayer();
+        if (playerInAttackRange && playerInSightRange) AttackPlayer();
     }
-
-    
-    void NextPosition()
-    {
-
-        _agent.SetDestination(FindObjectOfType<PlayerController>().transform.position);
-    }
-}
---
+++ Assets/NpcDamage.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NpcDamage : MonoBehaviour
+{
+    private void OnAnimatorIK(int layerIndex)
+    {
+      
+    }
+
+
+}
